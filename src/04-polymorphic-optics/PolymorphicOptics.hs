fb14c92d6092e4d44fa3f144fd14c299907b3560
monadplus <arnauabella@gmail.com>
2020-01-06 22:04:46 +0100

    add directories

---
+++ src/04-polymorphic-optics/PolymorphicOptics.hs
@@ -0,0 +1,25 @@
+{-# OPTIONS_GHC -Wno-unused-imports #-}
+{-# OPTIONS_GHC -Wno-missing-signatures #-}
+{-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE FlexibleInstances   #-}
+{-# LANGUAGE RankNTypes          #-}
+{-# LANGUAGE RecordWildCards     #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell     #-}
+{-# LANGUAGE TypeApplications    #-}
+{-# LANGUAGE TypeFamilies        #-}
+
+module PolymorphicOptics where
+
+-------------------------------------
+
+import           Control.Applicative
+import           Control.Lens
+import           Data.Char
+import qualified Data.Map            as Map
+import qualified Data.Set            as Set
+import qualified Data.Text           as T
+
+-------------------------------------
+
+foo = undefined
